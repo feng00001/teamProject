6121783c25108569ad03e9e128564b1479763ce2
UKVKU90MRTUVQO2\Administrator <m18642638097@163.com>
2017-04-14 14:18:55 +0800

    韩旭

--- project/todo/store/mutations.js
+++ project/todo/store/mutations.js
@@ -15,15 +15,19 @@ export const state = {
   shoplist: [],
   // 单个shop信息
   shopDetail: {},
-  classifyImg: ['http://k21.iblimg.com/prd/images/category/2016/09/进口面膜.jpg','assets/images/search.png'],
   // 测试用
   testImg: ['assets/imgs/logo.png','assets/imgs/11.jpg','assets/imgs/nav.png','assets/imgs/2.jpg'],
   // 高洋用 begin
   // gyimg: []
   // 高洋用 end
   // 王春园用 begin
-  userphone: 'assets/imgs/userphone.png'
+  userphone: 'assets/imgs/userphone.png',
   // 王春园用 end
+  // 韩旭 begin
+  classifyImg: ['http://k21.iblimg.com/prd/images/category/2016/09/进口面膜.jpg','assets/imgs/search.png'],
+  isHide: 'none',
+  cartImg: 'http://m.bl.com/h5-web/ui/h5resource/css/i/h5-cart-img_r2_c2.png'
+  //韩旭 
 }
 
 // 这里对state进行操作，对state的操作最好放在这里，不要私自动用state
@@ -34,6 +38,9 @@ export const mutations = {
   setShopDetail (state, test) {
   	state.shopDetail = test
   	console.log("name:"+test.shopname)
+  },
+  setIsHide(state) {
+    state.isHide = state.isHide === "none" ? "block" : "none"
   }
 }
 
