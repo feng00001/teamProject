a413750d7baf4a404af024b01a92f3a954d0b7ef
UKVKU90MRTUVQO2\Administrator <m18642638097@163.com>
2017-04-24 09:32:49 +0800

    han

--- project/todo/components/Classify.vue
+++ project/todo/components/Classify.vue
@@ -1,21 +1,7 @@
 
 <template>
   <div class="classify">
-  	<div id="header">
-  		<input type="text" placeholder="搜索你想找的商品"/>
-  		<img :src="$store.state.classifyImg[1]">
-  		<span>
-  			<p @touchend="switchoverNav" v-if="$store.state.isHide==='none'">...</p>
-  			<p @touchend="switchoverNav" v-else>x</p>
-  			<ul ref="nav" :style="{display:$store.state.isHide}">
-  				<router-link to="/shopList" tag="li" active-class="special"><i>1</i><b>首页</b></router-link>
-		    	<router-link to="/classify" tag="li" active-class="special"><i>1</i><b>搜索</b></router-link>
-		    	<router-link to="/classify" tag="li" active-class="special"><i>1</i><b>分类</b></router-link>
-		    	<router-link to="/order" tag="li" active-class="special"><i>1</i><b>购物车</b></router-link>
-		    	<router-link to="/mine" tag="li" active-class="special"><i>1</i><b>我的</b></router-link>
-  			</ul>
-  		</span>
-  	</div>
+  	<search-nav></search-nav>
   	<div id="content1">
 			 <div class="left">
 				<ol>
@@ -42,6 +28,7 @@
 
 <script>
 import BottomNav from './BottomNav.vue'
+import SearchNav from './SearchNav.vue'
 import {mapMutations, mapActions} from 'vuex'
 import $ from 'jquery'
 
@@ -58,9 +45,6 @@ export default {
 		this.changeUl(1);
 	},
 	methods: {
-		switchoverNav () {
-			this.$store.commit('setIsHide')
-		},
 		changeUl (typeid) {
 			var that = this;
 			$.ajax({
@@ -77,89 +61,21 @@ export default {
 		}
 	},
 	components: {
-		BottomNav
+		BottomNav,
+		SearchNav
 	}
 }
 </script>
 
 <style scoped>
-    #header{
-    	padding:.3rem 0;
-    	overflow: hidden;
-    }
-    #header input{
-	    height: .6rem;
-	    line-height: .6rem;
-	    border-bottom: 1px solid #ddd;
-	    float: left;
-	    background: #f4f4f4;
-	    border-radius: .1rem;
-	    width: 65%;
-	    margin-left: .3rem;
-	    padding-left:.3rem;
-	    background:#fff;
-    }
-    #header img{
-    	position: absolute;
-    	left:0.25rem;
-    	top:.2rem;
-    }
-    #header span{
-    	position: absolute;
-    	right:0.35rem;
-    	top:.1rem;
-    	
-    }
-    #header span ul::after{
-    	display: block;
-    	content:"";
-    	border-bottom:.15rem solid rgba(0,0,0,.8);
-    	border-left:.15rem solid white;
-    	border-right:.15rem solid white;
-    	position: absolute;
-    	right:.38rem;
-    	top:-.15rem;
-    }
-    #header span p{
-    	font-size:38px;
-    	position: absolute;
-    	left:-0.5rem;
-    	top:-0.35rem;
-    }
-    #header span ul{
-    	position: absolute;
-    	width:7.5rem;
-    	left:-7.15rem;
-    	top:1.1rem;
-    	border-bottom: 0.05rem solid #fe8477;
-        background-color: rgba(0,0,0,.8);
-        display: none;
-    }
-    #header span ul li{
-    	float:left;
-    	width:20%;
-    }
-    #header span ul li i{
-    	height:.5rem;
-    	padding:0 .3rem;
-    	line-height: .5rem;
-    }
-    #header span ul li b{
-    	height:.5rem;
-    	line-height: .5rem;
-    	padding:0 .3rem;
-    	display: block;
-    	color:white;
-    	font-weight: 100;
-    }
-	#content1{
-		display: flex;
-		flex-direction: row;
-		background: green;
-		width:100%;
-		overflow: auto;
-		height: 12rem;
-	}
+    #content1{
+	    display: flex;
+	    flex-direction: row;
+	    background: green;
+	    width:100%;
+	    overflow: auto;
+	    height: 12rem;
+	  }
 	#content1 .left{
 		width: 25%;
 	    background: #eee;
---
+++ project/todo/components/SearchNav.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="search-nav">
+    <div id="header">
+      <input type="text" placeholder="搜索你想找的商品"/>
+      <img :src="$store.state.classifyImg[1]">
+      <span>
+        <p @touchend="switchoverNav" v-if="$store.state.isHide==='none'">...</p>
+        <p @touchend="switchoverNav" v-else>x</p>
+        <ul ref="nav" :style="{display:$store.state.isHide}">
+          <router-link to="/shopList" tag="li" active-class="special"><i>1</i><b>首页</b></router-link>
+          <router-link to="/classify" tag="li" active-class="special"><i>1</i><b>搜索</b></router-link>
+          <router-link to="/classify" tag="li" active-class="special"><i>1</i><b>分类</b></router-link>
+          <router-link to="/order" tag="li" active-class="special"><i>1</i><b>购物车</b></router-link>
+          <router-link to="/mine" tag="li" active-class="special"><i>1</i><b>我的</b></router-link>
+        </ul>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapActions} from 'vuex'
+
+export default {
+  name: 'search-nav',
+  methods: {
+    switchoverNav () {
+      this.$store.commit('setIsHide')
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+    #header{
+      padding:.3rem 0;
+      overflow: hidden;
+    }
+    #header input{
+      height: .6rem;
+      line-height: .6rem;
+      border-bottom: 1px solid #ddd;
+      float: left;
+      background: #f4f4f4;
+      border-radius: .1rem;
+      width: 65%;
+      margin-left: .3rem;
+      padding-left:.3rem;
+      background:#fff;
+    }
+    #header img{
+      position: absolute;
+      left:0.25rem;
+      top:.2rem;
+    }
+    #header span{
+      position: absolute;
+      right:0.35rem;
+      top:.1rem;
+      
+    }
+    #header span ul::after{
+      display: block;
+      content:"";
+      border-bottom:.15rem solid rgba(0,0,0,.8);
+      border-left:.15rem solid white;
+      border-right:.15rem solid white;
+      position: absolute;
+      right:.38rem;
+      top:-.15rem;
+    }
+    #header span p{
+      font-size:38px;
+      position: absolute;
+      left:-0.5rem;
+      top:-0.35rem;
+    }
+    #header span ul{
+      position: absolute;
+      width:7.5rem;
+      left:-7.15rem;
+      top:1.1rem;
+      border-bottom: 0.05rem solid #fe8477;
+        background-color: rgba(0,0,0,.8);
+        display: none;
+    }
+    #header span ul li{
+      float:left;
+      width:20%;
+    }
+    #header span ul li i{
+      height:.5rem;
+      padding:0 .3rem;
+      line-height: .5rem;
+    }
+    #header span ul li b{
+      height:.5rem;
+      line-height: .5rem;
+      padding:0 .3rem;
+      display: block;
+      color:white;
+      font-weight: 100;
+    }
+  
+</style>
