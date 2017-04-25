a99eb35e7ea5c9b6a20be377d1dab5f1a38e6951
UKVKU90MRTUVQO2\Administrator <m18642638097@163.com>
2017-04-25 14:32:51 +0800

    han

--- project/dao/cartcountdao.js
+++ project/dao/cartcountdao.js
@@ -7,18 +7,29 @@ var pool = mysql.createPool($conf.mysql);
 var util = require('../../util/common');
 
 module.exports = {
-	insertcount: function (req, res, next) {
+	updatecount: function (req, res, next) {
 		pool.getConnection(function(err, connection) {
 			// 获取前台页面传过来的参数
 			var param = req.query || req.params;
 			// 建立连接，向表中插入值
-			connection.query($sql.sqlClassify01, null, function(err, result) {
+			connection.query($sql.sqlCart01, [param.shopid,param.userid], function(err, result) {
 
 				// 以json形式，把操作结果返回给前台页面
-				util.jsonWrite(res, result);
-
+				
+				var cart = result;
+				if(cart.length > 0){
+					connection.query($sql.sqlCart03, [param.shopid,param.userid], function(err, result) {
+						util.jsonWrite(res, result);
+						connection.release();
+					})
+				}else{
+					connection.query($sql.sqlCart02, [param.shopid,param.userid], function(err, result) {
+						util.jsonWrite(res, result);
+						connection.release();
+					})
+				}
 				// 释放连接 
-				connection.release();
+				
 			});
 		});
 	}
