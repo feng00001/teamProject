5832ae66ae66824eb8d89c3728d9f3cd6d0d9db1
soul0409 <523843931@qq.com>
2017-04-28 15:12:48 +0800

    no message

--- project/todo/components/Parameter.vue
+++ project/todo/components/Parameter.vue
@@ -4,37 +4,33 @@
       <li class="first">基本信息</li>
       <li>
         <span class="left l">种类</span>
-        <span class="right l">种类</span>
+        <span class="right l">洗护</span>
       </li>
       <li>
         <span class="left l">产地</span>
-        <span class="right l">产地</span>
+        <span class="right l">China</span>
       </li>
       <li>
-        <span class="left l">种类</span>
-        <span class="right l">种类</span>
+        <span class="left l">功效</span>
+        <span class="right l">修复,深层洁净,控油平衡,去头屑</span>
       </li>
       <li>
-        <span class="left l">产地</span>
-        <span class="right l">产地</span>
+        <span class="left l">适宜</span>
+        <span class="right l">男女通用</span>
       </li>
       <li>
-        <span class="left l">种类</span>
-        <span class="right l">种类</span>
+        <span class="left l">进口</span>
+        <span class="right l">国产</span>
       </li>
       <li>
-        <span class="left l">产地</span>
-        <span class="right l">产地</span>
+        <span class="left l">包装</span>
+        <span class="right l">瓶装</span>
       </li>
+      
       <li class="second">规格参数</li>
       <li>
-        <span class="left l">产地</span>
-        <span class="right l">产地</span>
-      </li>
-      <li class="second">产地</li>
-      <li>
-        <span class="left l">产地</span>
-        <span class="right l">产地</span>
+        <span class="left l">容量</span>
+        <span class="right l">750ml</span>
       </li>
 
     </ul>
